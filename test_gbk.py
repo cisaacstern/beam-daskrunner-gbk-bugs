86e69e14b02111e707d1f3b5019403ed7753f085
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-11-07 12:54:11 -0800

    unpartitioned create mock

--- test_gbk.py
+++ test_gbk.py
@@ -1,15 +1,30 @@
 import json
 import pathlib
 import secrets
+from unittest.mock import patch
 
 import apache_beam as beam
+import dask.bag as db
 import pytest
-from apache_beam.runners.dask.dask_runner import DaskRunner
+from apache_beam.runners.dask.dask_runner import DaskRunner, TRANSLATIONS
+from apache_beam.runners.dask.overrides import _Create
+from apache_beam.runners.dask.transform_evaluator import Create as DaskCreate, OpInput
 from apache_beam.testing import test_pipeline
 from apache_beam.testing.util import assert_that, equal_to
 
+# next steps:
+#    patch string keys with unpartitioned create
+#    show the way that assert_that doesn't work for basic things
+#    but it does with unpartitioned create
+
 runners = ["DirectRunner", DaskRunner()]
 runner_ids = ["DirectRunner", "DaskRunner"]
+
+
+class UnpartitionedCreate(DaskCreate):
+    def apply(self, input_bag: OpInput) -> db.Bag:
+        partitioned = super().apply(input_bag)
+        return partitioned.repartition(npartitions=1)
 
 
 def json_dump(element, tmpdir: pathlib.Path):
@@ -42,10 +57,21 @@ def test_gbk(
     expected_gbk: list[tuple],
     tmp_path_factory: pytest.TempPathFactory,
 ):
+
     tmpdir = tmp_path_factory.mktemp("tmp")
-    with test_pipeline.TestPipeline(runner=runner) as p:
-        pcoll = p | beam.Create(collection) | beam.GroupByKey()
-        pcoll | beam.Map(json_dump, tmpdir=tmpdir)
+
+    NEW_TRANSLATIONS = TRANSLATIONS.copy()
+    NEW_TRANSLATIONS[_Create] = UnpartitionedCreate
+    with patch("apache_beam.runners.dask.dask_runner.TRANSLATIONS", NEW_TRANSLATIONS) as nt:
+        with test_pipeline.TestPipeline(runner=runner) as p:
+            pcoll = p | beam.Create(collection) | beam.GroupByKey()
+            pcoll | beam.Map(json_dump, tmpdir=tmpdir)
+
+        for k in NEW_TRANSLATIONS:
+            if not k == _Create:
+                assert NEW_TRANSLATIONS[k] == TRANSLATIONS[k]
+            else:
+                assert NEW_TRANSLATIONS[k] == UnpartitionedCreate
 
     global_window = []
     for fname in tmpdir.iterdir():
